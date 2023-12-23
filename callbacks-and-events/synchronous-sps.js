9c06070e863fe83de3eb9b16341a23cd31a3de87
Sergei-Nikiforov <pclp@inbox.ru>
2023-12-23 21:03:24 +0300

    The continuation-passing style

---
+++ callbacks-and-events/synchronous-sps.js
@@ -0,0 +1,9 @@
+// The continuation-passing style
+
+function addCps (a, b, callback) {
+    callback(a + b);
+}
+
+console.log('before');
+addCps(1, 2, result => console.log(`Result: ${result}`));
+console.log('after');
