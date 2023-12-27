64ccefa7b13c93080f072184725f9518ccdda44c
Sergei-Nikiforov <pclp@inbox.ru>
2023-12-27 23:33:04 +0300

    using the EventEmitter

--- callbacks-and-events/uncaught-exceptions.js
+++ callbacks-and-events/uncaught-exceptions.js
@@ -10,6 +10,18 @@ function readJSONThrows(filename, callback) {
     });
 }
 
+process.on('uncaughtException', (err) => {
+    console.error(`'uncaughtException: ${err.message}`)
+    // Terminates the application with 1 (error) as exit code.
+    // Without the following line, the application would continue
+    process.exit(1);
+})
+.on('unhandledRejection', (err) => {
+    console.error(`'unhandledRejection: ${err.message}`)
+})
+
+nonexistentFunc();
+
 try {
     readJSONThrows('invalid_json.json', (err, data) => console.error(err));
 } catch (error) {
