f59b172950cd8cb53f6284b110a99b3dcb7605d1
Sergei-Nikiforov <pclp@inbox.ru>
2023-12-30 23:37:47 +0300

    EventEmitter versus callbacks

---
+++ callbacks-and-events/event-emiter-vs-callback.js
@@ -0,0 +1,17 @@
+import { EventEmitter } from 'events';
+
+function helloEvents () {
+    const eventEmitter = new EventEmitter();
+
+    setTimeout(() => eventEmitter.emit('complete', 'hello world'), 100);
+    process.nextTick(() => eventEmitter.emit('complete', 'hello world'));
+
+    return eventEmitter
+}
+
+function helloCallback (cb) {
+    setTimeout(() => cb(null, 'hello world'), 100);
+}
+
+helloEvents().on('complete', message => console.log(message));
+helloCallback((err, message) => console.log(message));
